28520a26748b94e76d9524aa8522f8b1ab874302
Darkness1908 <2002phone2002@gmail.com>
2025-10-02 05:51:48 +0300

    third commit

--- src/GameAttributes.java
+++ src/GameAttributes.java
@@ -3,18 +3,20 @@ import java.util.Set;
 
 public class GameAttributes {
     private final char[][] matrix;
-    private final int nn;
+    private final int N;
     private int count;
     private char currentPlayerColor;
-    int currentX;
-    int currentY;
+    private int firstPlayerCurrentX;
+    private int firstPlayerCurrentY;
+    private int secondPlayerCurrentX;
+    private int secondPlayerCurrentY;
     private String currentPlayer;
     private final Set<Pair> firstPlayerCheckers;
     private final Set<Pair> secondPlayerCheckers;
 
     public GameAttributes(int N, char color) {
         matrix = new char[N][N];
-        this.nn = N*N;
+        this.N = N;
         this.currentPlayer = "Первый";
         currentPlayerColor = color;
         firstPlayerCheckers = new HashSet<>();
@@ -22,34 +24,79 @@ public class GameAttributes {
         count = 0;
     }
 
-    public int getNn() {
-        return nn;
+    public int getN() {
+        return N;
     }
     public int getCount() {
         return count;
     }
-    public void incrementCount() {
-        count++;
-    }
     public void changeCurrentPlayer() {
         currentPlayer = currentPlayer.equals("Второй") ? "Первый": "Второй";
-        System.out.println(currentPlayer + " игрок делает ход");
     }
     public void setMatrixValue(int x, int y) {
-        if (count != 0) {
-            matrix[y][x] = currentPlayerColor;
-            (currentPlayer.equals("Первый") ? firstPlayerCheckers : secondPlayerCheckers)
-                    .add(new Pair(currentX, currentY));
+        if ((x < 0) || (x >= N) || (y < 0) || (y >= N)) {
+            throw new IllegalArgumentException("Координаты шашки выходят за пределы поля");
         }
-        currentX = x;
-        currentY = y;
+        if (count > 1) {
+            if (currentPlayer.equals("Первый")) {
+                firstPlayerCheckers.add(new Pair(firstPlayerCurrentX, firstPlayerCurrentY));
+                firstPlayerCurrentX = x;
+                firstPlayerCurrentY = y;
+            }
+            else {
+                secondPlayerCheckers.add(new Pair(secondPlayerCurrentX, secondPlayerCurrentY));
+                secondPlayerCurrentX = x;
+                secondPlayerCurrentY = y;
+            }
+
+        }
+        else {
+            if (currentPlayer.equals("Первый")) {
+                firstPlayerCurrentX = x;
+                firstPlayerCurrentY = y;
+            } else {
+                secondPlayerCurrentX = x;
+                secondPlayerCurrentY = y;
+            }
+        }
+        matrix[y][x] = currentPlayerColor;
         count++;
+        for (Pair p : firstPlayerCheckers) {
+            System.out.println("Значения в сете 1: "  +p.x + " " + p.y);
+        }
+        for (Pair p : secondPlayerCheckers) {
+            System.out.println("Значения в сете 2: "  +p.x + " " + p.y);
+        }
     }
     public char getMatrixValue(int x, int y) {
         return matrix[y][x];
     }
     public void changeCurrentPlayerColor() {
         currentPlayerColor = currentPlayerColor == 'W' ? 'B' : 'W';
+    }
+    public char getCurrentPlayerColor() {
+        return currentPlayerColor;
+    }
+    public String getCurrentPlayer() {
+        return currentPlayer;
+    }
+    public Set<Pair> getFirstPlayerCheckers() {
+        return firstPlayerCheckers;
+    }
+    public Set<Pair> getSecondPlayerCheckers() {
+        return secondPlayerCheckers;
+    }
+    public int getFirstPlayerCurrentX() {
+        return firstPlayerCurrentX;
+    }
+    public int getFirstPlayerCurrentY() {
+        return firstPlayerCurrentY;
+    }
+    public int getSecondPlayerCurrentX() {
+        return secondPlayerCurrentX;
+    }
+    public int getSecondPlayerCurrentY() {
+        return secondPlayerCurrentY;
     }
 
     public static class Pair {
@@ -60,5 +107,19 @@ public class GameAttributes {
             this.x = x;
             this.y = y;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;              // если это тот же объект
+            if (o == null || getClass() != o.getClass()) return false; // если null или другой класс
+            Pair pair = (Pair) o;
+            return x == pair.x && y == pair.y;       // сравниваем координаты
+        }
+
+        @Override
+        public int hashCode() {
+            return 31 * x + y;                       // простая хэш-функция для пары
+        }
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,30 +12,59 @@ public class Main {
             }
         }
         checkParametersNumber(6, args.length);
-
         GameOptions gameOptions = new GameOptions(args);
-
-        System.out.println("Новая игра начата");
         startGame(gameOptions);
     }
 
     public static void startGame(GameOptions gm) {
+        System.out.println("Новая игра началась");
         Scanner sc = new Scanner(System.in);
         GameAttributes ga = new GameAttributes(gm.getN(), gm.getFirstPlayerColor());
         while (ga.getCount() < 6) {
             makeMove(sc, ga);
+            ga.changeCurrentPlayer();
+            ga.changeCurrentPlayerColor();
         }
-        while (ga.getCount() < ga.getNn()) {
+        while (ga.getCount() < ga.getN()*ga.getN()) {
             makeMove(sc, ga);
             checkGameStatus(ga);
+            ga.changeCurrentPlayer();
+            ga.changeCurrentPlayerColor();
         }
+        System.out.println("Игра окончена. Ничья");
     }
 
     public static void checkGameStatus(GameAttributes ga) {
+        if (ga.getCurrentPlayer().equals("Первый")) {
+            rotate90(ga, ga.getFirstPlayerCheckers(), ga.getFirstPlayerCurrentX(), ga.getFirstPlayerCurrentY());
+        }
+        else rotate90(ga, ga.getSecondPlayerCheckers(), ga.getSecondPlayerCurrentX(), ga.getSecondPlayerCurrentY());
+    }
 
+    public static void rotate90(GameAttributes ga, Set<GameAttributes.Pair> playerCheckers, int x, int y) {
+        for (GameAttributes.Pair p : playerCheckers) {
+            System.out.println(p.x + " " + p.y);
+            float dX = (float) (x - p.x) / 2;
+            float dY = (float) (y - p.y) / 2;
+            float cX = (float) (x + p.x) / 2;
+            float cY = (float) (y + p.y) / 2;
+            if ((x-p.x)*(x-p.x) + (y-p.y)*(y-p.y) % 2 == 0) {
+                if (playerCheckers.contains(
+                        new GameAttributes.Pair((int) (cX+(-1)*dY), (int) (cY+dX)))) {
+                    System.out.println("Первое условие");
+                    if (playerCheckers.contains(
+                            new GameAttributes.Pair((int) (cX+dY), (int) (cY+(-1)*dX)))) {
+                        System.out.println("Игра окончена. Игрок " + ga.getCurrentPlayerColor()
+                                + " одержал победу");
+                        System.exit(0);
+                    }
+                }
+            }
+        }
     }
 
     public static void makeMove(Scanner sc, GameAttributes ga) {
+        System.out.println(ga.getCurrentPlayer() + " игрок делает ход");
         String input = sc.nextLine();
         String[] params = input.split(" ");
         checkParametersNumber(3, params.length);
@@ -45,10 +75,8 @@ public class Main {
             throw new RuntimeException("На этой клетке уже присутствует шашка");
         }
         else ga.setMatrixValue(x, y);
-        ga.changeCurrentPlayer();
-        ga.changeCurrentPlayerColor();
-        ga.incrementCount();
     }
+
     public static void checkParametersNumber(int lenReq, int lenProv) {
         if (lenReq != lenProv) {
             throw new RuntimeException("Параметров должно быть ровно " + lenReq);
